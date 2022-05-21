3ce7403ebfe2996fc7bb0ca53e8174c1efc883de
tsx0314 <tian.shixing2001@gmail.com>
2022-05-21 11:57:38 +0800

    Add config and add components

---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDaF0vOPqgdsct-JDwNuaac2lDsqItuWSk",
+    authDomain: "budgie-f15e1.firebaseapp.com",
+    projectId: "budgie-f15e1",
+    storageBucket: "budgie-f15e1.appspot.com",
+    messagingSenderId: "732417771593",
+    appId: "1:732417771593:web:563b16452bf6694b0e9798",
+    measurementId: "G-PTN047SNK5"
+  };
