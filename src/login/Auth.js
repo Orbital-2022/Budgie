3a308e7041a84eb6117528440b5584e43d42e148
xiaoge26 <liuxiaoge0206@qq.com>
2022-05-26 23:53:44 +0800

    changed some text

--- src/login/Auth.js
+++ src/login/Auth.js
@@ -23,7 +23,7 @@ export default function Auth() {
   return (
     <div className="row flex flex-center">
       <div className="col-6 form-widget" aria-live="polite">
-        <h1 className="header">Supabase + React</h1>
+        <h1 className="header">Budgie</h1>
         <p className="description">Sign in via magic link with your email below</p>
         {loading ? (
           'Sending magic link...'
