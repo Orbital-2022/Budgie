4886910838ea97127d2a0e30f1c0681eae197649
tsx0314 <tian.shixing2001@gmail.com>
2022-05-26 16:21:04 +0800

    Solve conflicts

--- src/config/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_PUBLIC_KEY
-)
-
-export { supabase }
