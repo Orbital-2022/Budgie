5ad7d2e45d07c0c8bbc0ac19cef0042aa231394a
tsx0314 <tian.shixing2001@gmail.com>
2022-06-14 12:43:32 +0800

    record table

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,16 +1,44 @@
-import { Button } from "@mui/material";
 import React from "react";
 import { supabase } from "../config/supabaseClient";
 import styles from "./MainPage.module.css";
 import { useNavigate } from "react-router-dom";
 import {useEffect, useState } from "react";
 import Box from "../components/Box";
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+  Button
+} from '@mui/material'
+
 
 function IncomeCard() {
+  const [data, setData] = useState([]);
+  const user = supabase.auth.user();
 
+  useEffect(() => {fetchData().catch(console.error);}, []);
+  const fetchData = async () => {
+    let { data: income, error } = await supabase
+        .from('incomes')
+        .select("*")
+        .eq('user_id',user.id)
+
+    if (error) console.log("error", error);
+    else setData(income);
+  };
+  
+  let sum = 0;
+
+  for (let index = 0; index < data.length; index++) {
+  sum += data[index].amount;
+  }
   return (
       <Box>
-          <h2>Total income:</h2>
+          <h2>Total income:{sum}</h2>
       </Box>
   )
 }
@@ -43,6 +71,51 @@ function ExpenseCard() {
   )
 }
 
+function RecordsTable(){
+  const [data, setData] = useState([]);
+  const user = supabase.auth.user();
+
+  useEffect(() => {fetchData().catch(console.error);}, []);
+  const fetchData = async () => {
+    let { data: expense, error } = await supabase
+        .from('expenses')
+        .select("*")
+        .eq('user_id',user.id)
+
+    if (error) console.log("error", error);
+    else setData(expense);
+  };
+
+  return (
+    <TableContainer sx={{ maxHeight: '300px' }} component={Paper}>
+      <Table stickyHeader aria-label='simple table'>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell>Date</TableCell>
+            <TableCell>Category</TableCell>
+            <TableCell>Amount</TableCell>
+            <TableCell>Remarks</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(row => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+               <TableCell></TableCell>
+              <TableCell>{row.expense_date}</TableCell>
+              <TableCell>{row.category}</TableCell>
+              <TableCell>{row.amount}</TableCell>
+              <TableCell align='center'>{row.remark}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
   
 export function CardsAndTable() {
 
@@ -50,7 +123,8 @@ export function CardsAndTable() {
     <div className={styles.container}>
     <h1>Your Balance: </h1>
     <IncomeCard />
-    <ExpenseCard />     
+    <ExpenseCard />    
+    <RecordsTable /> 
      </div>
 
   );
